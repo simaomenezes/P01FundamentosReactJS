d355bd77ecbddd8ce5d84badda142728a6d144b5
Simão Neto <simaomenezesneto@gmail.com>
2023-03-01 22:34:46 +0000

    feat: Component Sidebar and your styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,35 @@
 import { Header } from "./components/Header"
 import { Post } from "./Post"
 
+import styles from './App.module.css'
+
 import './global.css'
+import { Sidebar } from "./components/Sidebar"
 
 function App() {
   return (
     <div>
       <Header/>
-      <Post
-        author="Fulano de tal"
-        content="Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet"
-      />
+      <div className={styles.wrapper}>
+        <Sidebar/>
+        <main>
+          <Post
+            author="Lorem II"
+            content="Aute ipsum amet consequat officia pariatur laborum aute nulla pariatur ullamco. Cupidatat veniam cillum excepteur qui ad ad elit consequat magna duis laborum est exercitation deserunt. Occaecat ad anim aliqua culpa enim nulla elit in exercitation voluptate est."
+          />
 
-      <Post
-        author="Fulano de tal"
-        content="Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet"
-      />
+          <Post
+            author="Lorem II"
+            content="Aute ipsum amet consequat officia pariatur laborum aute nulla pariatur ullamco. Cupidatat veniam cillum excepteur qui ad ad elit consequat magna duis laborum est exercitation deserunt. Occaecat ad anim aliqua culpa enim nulla elit in exercitation voluptate est."
+          />
+
+          <Post
+            author="Lorem II"
+            content="Aute ipsum amet consequat officia pariatur laborum aute nulla pariatur ullamco. Cupidatat veniam cillum excepteur qui ad ad elit consequat magna duis laborum est exercitation deserunt. Occaecat ad anim aliqua culpa enim nulla elit in exercitation voluptate est."
+          />
+        </main>
+
+      </div>
     </div>
   )
 }
