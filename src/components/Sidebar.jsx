6493c311a4c2f3509d1e210beddcfd109b7682c9
Simão Neto <simaomenezesneto@gmail.com>
2023-03-09 20:37:45 +0000

    feat: work css,  Done sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,3 +1,5 @@
+import { PencilLine } from 'phosphor-react'
+
 import styles from './Sidebar.module.css'
 
 export function Sidebar() {
@@ -8,12 +10,14 @@ export function Sidebar() {
                 src="https://images.unsplash.com/photo-1577375729078-820d5283031c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=50"/> 
 
             <div className={styles.profile}>
+                <img className={styles.avatar} src='https://avatars.githubusercontent.com/u/625433?s=96&v=4'/>
                 <strong>Tryonn Neto</strong>
                 <span>Web Developer</span>
             </div>
 
             <footer>
                 <a href='#'>
+                    <PencilLine/>
                     Editar seu perfil
                 </a>
             </footer>
