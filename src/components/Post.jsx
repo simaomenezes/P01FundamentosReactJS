f6d3063d74c2494c5e08f2b4d26faa59e1db4c0c
Simão Neto <simaomenezesneto@gmail.com>
2023-03-09 21:30:24 +0000

    feat: Work css of Posts

---
+++ src/components/Post.jsx
@@ -0,0 +1,31 @@
+import styles from './Post.module.css'
+
+export function Post() {
+    return (
+        <article className={styles.post}>
+            <header>
+                <div className={styles.author}>
+                    <img className={styles.avatar} src='https://avatars.githubusercontent.com/u/625433?s=96&v=4'/>
+                    <div className={styles.authorInfo}>
+                        <strong>Simão Neto</strong>
+                        <span>Web Developer</span>
+                    </div>
+                </div>
+                <time title='09 de março às 20:51' dateTime='2023-03-09 20:51'>Publicado há 1h</time>
+            </header>
+
+            <div className={styles.content}>
+            <p>Fala galeraa 👋</p>
+
+            <p>Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀</p>
+
+            <p>👉 {' '}  <a href=''>jane.design/doctorcare</a> {' '} 
+                <a href=''>#novoprojeto</a>{' '} 
+                <a href=''>#nlw</a>{' '} 
+                <a href=''>#rocketseat</a>
+            </p> 
+
+            </div>
+        </article>
+    )
+}
